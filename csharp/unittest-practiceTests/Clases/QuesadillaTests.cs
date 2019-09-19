e068511047e219eda019458e5225f6b24c6c0c3d
Alvaroc <alvaro,nayru@gmail.com>
2019-09-19 00:18:11 -0500

    Added 90% of unit-tests

---
+++ csharp/unittest-practiceTests/Clases/QuesadillaTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using unittestpractice.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unittestpractice.Clases.Tests
+{
+    [TestClass()]
+    public class QuesadillaTests
+    {
+        //TODO ARTURO
+           //Asserts
+        
+
+        [TestMethod()]
+        public void prepareSingleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void prepareDoubleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getQuesoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setQuesoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getTortillaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setTortillaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getHeatLevelTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setHeatLevelTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ csharp/unittest-practiceTests/Clases/QuesoChihuahaTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using unittestpractice.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unittestpractice.Clases.Tests
+{
+    [TestClass()]
+    public class QuesoChihuahaTests
+    {
+        //Asserts
+        QuesoChihuahua quesoChihuahua = new QuesoChihuahua();
+
+        [TestMethod()]
+        public void testCurrentTemperature()
+        {
+            quesoChihuahua.setCurrentTemperature(21);
+            Assert.AreEqual(21, quesoChihuahua.getCurrentTemperature());
+        }
+
+        [TestMethod()]
+        public void testFalseMelt()
+        {
+            quesoChihuahua.melt(false);
+            Assert.IsFalse(quesoChihuahua.isMelted());
+        }
+        [TestMethod()]
+        public void testTrueMelt()
+        {
+            quesoChihuahua.melt(true);
+            Assert.IsTrue(quesoChihuahua.isMelted());
+        }
+        [TestMethod()]
+        public void testMelting()
+        {
+            Assert.AreEqual(25, quesoChihuahua.getMeltingTemperature());
+        }
+    }
+}
