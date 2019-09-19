f288f9d2553c811ede9b75bfbea33d31cc8734d2
Arturo García <artur@MININT-I9J28G3.redmond.corp.microsoft.com>
2019-09-19 08:45:28 -0500

    Added missing files from C# test project

---
+++ csharp/unittest-practiceTests/Clases/QuesadillaTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+namespace unittestpractice.Clases.Tests
+{
+    [TestClass()]
+    public class QuesadillaTests
+    {
+        Quesadilla quesadilla;
+        Mock mockedQueso;
+        Mock mockedTortilla;
+        //TODO ARTURO
+        //Asserts
+        [TestInitialize]
+        public void setup()
+        {
+            quesadilla = new Quesadilla();
+            mockedQueso = new Mock<Queso>();
+            mockedTortilla = new Mock<Tortilla>();
+        }
+
+        [TestMethod()]
+        public void quesadillaPerfecta()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void prepareDoubleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getQuesoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setQuesoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getTortillaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setTortillaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getHeatLevelTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setHeatLevelTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
