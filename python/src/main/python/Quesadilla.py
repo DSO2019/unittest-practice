2e8c12d672334338e204bdf4359794f6df9532f6
JoseLuis <jltorrenterash@gmail.com>
2019-09-20 17:37:45 -0500

    Python double quesadilla

--- python/src/main/python/Quesadilla.py
+++ python/src/main/python/Quesadilla.py
@@ -1,9 +1,10 @@
 class Quesadilla:
-    def __init__(self, queso = None, tortilla = None, heatLevel = 0):
+    def __init__(self, queso = None, tortilla = None, heatLevel = 0, tortilla2 = None):
         self.queso = queso
         self.tortilla = tortilla
         self.heatLevel = heatLevel
-    
+        self.tortilla2 = tortilla2
+
     def getQueso(self):
         return self.queso
     
@@ -21,6 +22,12 @@ class Quesadilla:
     
     def setHeatLevel(self, heatLevel):
         self.heatLevel = heatLevel
+    
+    def getTortilla2(self):
+        return self.tortilla2
+    
+    def setTortilla2(self, tortilla):
+        self.tortilla2 = tortilla
     
     def prepareSingle(self):
         while(self.getQueso().getCurrentTemperature() < self.getQueso().getMeltingTemperature() and self.getTortilla().getCurrentTemperature() < self.getTortilla().getToastTemperature()):
@@ -40,3 +47,27 @@ class Quesadilla:
         else:
             return "You ran out of gas"
      
+    def prepareDouble(self):
+        while(self.getQueso().getCurrentTemperature() < self.getQueso().getMeltingTemperature() and self.getTortilla().getCurrentTemperature() < self.getTortilla().getToastTemperature() and self.getTortilla2().getCurrentTemperature() < self.getTortilla2().getToastTemperature()):
+            self.getTortilla().setCurrentTemperature(self.getTortilla().getCurrentTemperature() + self.getHeatLevel())
+            self.getTortilla2().setCurrentTemperature(self.getTortilla2().getCurrentTemperature() + self.getHeatLevel())
+            self.getQueso().setCurrentTemperature(self.getQueso().getCurrentTemperature() + self.getHeatLevel())
+            if (self.getTortilla().getCurrentTemperature() >= self.getTortilla().getToastTemperature()):
+                self.getTortilla().toast(True)
+            if (self.getTortilla2().getCurrentTemperature() >= self.getTortilla2().getToastTemperature()):
+                self.getTortilla2().toast(True)
+            if (self.getQueso().getCurrentTemperature() >= self.getQueso().getMeltingTemperature()):
+                self.getQueso().melt(True)
+        
+        if(self.getQueso().isMelted() and self.getTortilla().isToasted() and self.getTortilla2().isToasted()):
+            return "Perfect quesadilla"
+        if(self.getQueso().isMelted() and (self.getTortilla().isToasted() or self.getTortilla2().isToasted())):
+            return "Good quesadilla"
+        if(self.getQueso().isMelted() and not self.getTortilla().isToasted() and not self.getTortilla2().isToasted()):
+            return "Regular quesadilla"
+        if(not self.getQueso().isMelted() and self.getTortilla().isToasted() and self.getTortilla2().isToasted()):
+            return "Bad quesadilla"
+        if(not self.getQueso().isMelted() and (self.getTortilla().isToasted() or self.getTortilla2().isToasted())):
+            return "Terrible quesadilla"
+        else:
+            return "You ran out of gas"
