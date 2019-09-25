fb3a5d06f7b063bc96a0ec626b8595694e21b15c
Sebastián Aceves <sebastian@altomobile.io>
2019-09-25 16:05:49 -0500

    fix java checktyle

--- java/src/main/java/mx/iteso/ut/Tortilla.java
+++ java/src/main/java/mx/iteso/ut/Tortilla.java
@@ -1,10 +1,18 @@
 package mx.iteso.ut;
-
+/**Interfaz Tortilla.*/
 public interface Tortilla {
-    public boolean isToasted();
-    public int getToastTemperature();
-    public int getCurrentTemperature();
-    public void setCurrentTemperature(int temp);
-    public void toast(boolean toasted);
+    /**@return si la tortilla esta tostada.*/
+    boolean isToasted();
 
+    /**@return la temperatura a la que se tosta la tortilla.*/
+    int getToastTemperature();
+
+    /**@return la temperatura actual de la tortilla.*/
+    int getCurrentTemperature();
+
+    /**@param temp temperatuca actual.*/
+    void setCurrentTemperature(int temp);
+
+    /**@param toasted si la tortilla esta tostada.*/
+    void toast(boolean toasted);
 }
